19fdcd85a1c3c145ca88987975a336fa091464f5
Prabhasbots <Aniketkmrjha@gmail.com>
2025-03-13 22:01:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "26910777"))
 API_HASH = environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://envs.sh/Bji.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://vault.pictures/p/8bf0b544a4f64c3e9258faa0b5ec71af')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "SVMRIPPS_FILE_BOT") # without @
 PORT = environ.get("PORT", "8080")
